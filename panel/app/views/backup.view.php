8e2c45ab213098b0ed1c534f38eaa7429c898b99
TiBounise <contact@tibounise.com>
2013-03-04 20:30:16 +0100

    Began to work on the interface of the backup manager

--- panel/app/views/backup.view.php
+++ panel/app/views/backup.view.php
@@ -6,6 +6,31 @@
 
 <hr />
 
+<form action="processor.php?action=returnbackup" class="standard-form">
+	<h2>Sauvegarde des médias</h2>
+	<div class="input-control">
+		<div class="input-field">
+			<p class="input-label">Activer la sauvegarde des média <input type="checkbox" /></p>
+		</div>
+	</div>
+	<hr />
+	<h2>Sauvegarde des éléments</h2>
+	<div class="input-control">
+		<div class="input-field">
+			<p class="input-label">Activer la sauvegarde des éléments <input type="checkbox" /></p>
+		</div>
+	</div>
+	<div class="input-control">
+		<p class="input-label">Type de sauvegarde :</p>
+		<div class="input-field">
+			<select>
+				<option>Combinée (1 fichier)</option>
+				<option>Séparée (plusieurs fichiers)</option>
+			</select>
+		</div>
+	</div>
+</form>
+
 <?php
 	include 'assets/php/footer.view.php';
 ?>
