925f000be6357ad93cd21e44f7cd8853b33e7a06
TiBounise <contact@tibounise.com>
2013-03-06 16:23:59 +0100

    Follow #4. Remove useless functions

--- panel/lib/kompakt/helpers/sanitize.class.php
+++ panel/lib/kompakt/helpers/sanitize.class.php
@@ -2,18 +2,32 @@
 
 namespace Kompakt\Helpers;
 
+/**
+ * @package Kompakt\Helpers
+ */
 class Sanitize {
+	/**
+	 * Sanitzes an email adress
+	 * 
+	 * @param string $input Email to sanitize
+	 * @return string Sanitized email
+	 * @access public
+	 * @static
+	 */
 	public static function Email($input) {
 		return filter_var($input,FILTER_SANITIZE_EMAIL);
 	}
+
+	/**
+	 * Encodes a string to url format
+	 * 
+	 * @param string $input String to re-format
+	 * @return string Formatted string
+	 * @access public
+	 * @static
+	 */
 	public static function Urlencode($input) {
 		return filter_var($input,FILTER_SANITIZE_ENCODED);
-	}
-	public static function Float($input) {
-		return filter_var($input,FILTER_SANITIZE_NUMBER_FLOAT);
-	}
-	public static function Integer($input) {
-		return filter_var($input,FILTER_SANITIZE_NUMBER_INT);
 	}
 }
 
