8efbf71b3362e0eaebe37f9c62a5d518d3d673b4
TiBounise <contact@tibounise.com>
2013-04-08 22:12:29 +0200

    Cleaning mess.

--- panel/app/controllers/settings.controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$app->configureApp(ERROR | LOCALIZED | LOG_PROTECTION | CONFIG);
-$app->Config->pushJSON(file_get_contents('../config/admin.json'));
-$app->configureLocales('locales/'.$app->Config->getKey('language').'.json');
-
-$app->HtmlVars->setKey('title','settings');
-
-?>
--- panel/app/views/index.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'assets/php/header.view.php';
-?>
-
-<h1><?= $app->Locales->getKey('username'); ?></h1>
-
-<?php
-    require 'assets/php/footer.view.php';
-?>
