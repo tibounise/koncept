4d00ffb0abb769a43d44594c9477cd94ad4557c6
TiBounise <contact@tibounise.com>
2013-04-08 22:34:33 +0200

    Adding debug to the homepage

--- panel/app/controllers/index/index.controller.php
+++ panel/app/controllers/index/index.controller.php
@@ -5,6 +5,4 @@ $app->configureApp(USER_HANDLING | CONFIG | LOG_PROTECTION | LOCALIZED);
 $app->Config->pushJSON(file_get_contents('../config/admin.json'));
 $app->configureLocales('locales/'.$app->Config->getKey('language').'.json');
 
-$app->HtmlVars->setKey('title','home');
-
 ?>
--- panel/app/views/index/index.view.php
+++ panel/app/views/index/index.view.php
@@ -1,8 +1,12 @@
 <?php
+	$app->HtmlVars->setKey('title','home');
     require 'assets/php/header.view.php';
 ?>
 
-<h1><?= $app->Locales->getKey('username'); ?></h1>
+<h1>Welcome to koncept !</h1>
+<p>Version de PHP : <?= phpversion(); ?></p>
+<p>Include path : <?= get_include_path(); ?></p>
+<p>Fichier requis : <?php var_dump(get_required_files()); ?></p>
 
 <?php
     require 'assets/php/footer.view.php';
