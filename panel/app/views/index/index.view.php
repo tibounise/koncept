1dc419886cc6aaf7ed76e34aaa6d96998db7267b
TiBounise <contact@tibounise.com>
2013-04-11 14:51:32 +0200

    New homepage, removing the image. Closes #11

--- panel/app/views/index/index.view.php
+++ panel/app/views/index/index.view.php
@@ -1,16 +1,35 @@
 <?php
 	$app->HtmlVars->setKey('title','home');
-	$app->HtmlVars->setKey('frame-class','blueprint');
-    require 'assets/php/header.view.php';
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>koncept:<?= $app->HtmlVars->getKey('title'); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet/less" href="assets/css/home.less" />
+        <link rel="stylesheet" href="assets/css/font-awesome.css" />
+        <script src="assets/js/less/less-1.3.3.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="header-bar">
+            <div class="logo">
+                <a href="index.php"><img src="assets/images/koncept-logo.svg" height="35" width="185" /></a>            </div>
+            <div class="icon-bar">
+                <a href="settings.php" class="icon" title="<?= $app->Locales->getKey('settings'); ?>"><i class="icon-cogs"></i></a>
+                <a href="backup.php" class="icon" title="<?= $app->Locales->getKey('backup'); ?>"><i class="icon-umbrella"></i></a>
+                <a href="login.php?logout=true&token=<?= Kompakt\Handlers\User::getToken(); ?>" class="icon" title="<?= $app->Locales->getKey('logout'); ?>"><i class="icon-signout"></i></a>
+            </div>
+        </div>
 
-<div class="info-container">
-	<h1 class="center"><?= $app->Locales->getKey('welcome'); ?></h1>
-	<p>Version de PHP : <?= phpversion(); ?></p>
-	<p>Include path : <?= get_include_path(); ?></p>
-	<p>Fichier requis : <?= count(get_required_files()); ?></p>
-</div>
-
-<?php
-    require 'assets/php/footer.view.php';
-?>
+        <div class="home-frame">
+			<div class="flat-container">
+				<a href="element.php?action=list"><div class="item"><i class="icon-file"></i></div></a>
+				<a href="media.php?action=list"><div class="item"><i class="icon-camera"></i></div></a>
+				<a href="router.php?action=list"><div class="item"><i class="icon-sitemap"></i></div></a>
+				<a href="settings.php"><div class="item"><i class="icon-cogs"></i></div></a>
+			</div>
+		</div>
+	</body>
+</html>
