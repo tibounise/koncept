5cf6a10e1d4e8d3ad96cf4e1ab02328397506dab
Jean Thomas <tibounise@gmail.com>
2013-04-17 07:24:21 +0200

    Added session starter.

--- app/lib/Deone/Deone/Handlers/Deone.php
+++ app/lib/Deone/Deone/Handlers/Deone.php
@@ -3,7 +3,11 @@
 namespace Deone\Handlers;
 
 class Deone {
-	public function getFetcher() {
+	public function __construct() {
+		session_start();
+	}
+
+	public static function getFetcher() {
 		return new \Deone\Handlers\Fetcher;
 	}
 }
